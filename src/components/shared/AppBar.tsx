89628b86125c45df1b6a6774dd2ef2bd47757521
Kim Bell <krbell4@gmail.com>
2026-03-18 19:00:37 -0400

    Migrate AppBar component to MDB React
    - Convert navbar classes to MDBNavbar component with expand, light, bgColor props
    - Replace navbar-brand anchor with MDBNavbarBrand component
    - Maintain existing utility classes for positioning and styling
    - Import MDBNavbar, MDBNavbarBrand from mdb-react-ui-kit
    
    AppBar now uses MDB React components while other components still use Bootstrap classes

--- src/components/shared/AppBar.tsx
+++ src/components/shared/AppBar.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { MDBNavbar, MDBNavbarBrand } from 'mdb-react-ui-kit';
 
 const AppBar = (): React.JSX.Element => {
-  return(
+  return (
     <div>
-      <nav className="navbar navbar-expand-lg text-white bg-primary navbar-dark">
-        <a href="/" className="navbar-brand pl-2">
+      <MDBNavbar expand="lg" light bgColor="primary" className="navbar-dark">
+        <MDBNavbarBrand href="/" className="pl-2">
           <span className="text-white pl-3 font-weight-light">Memory Game</span>
-        </a>
-      </nav>
+        </MDBNavbarBrand>
+      </MDBNavbar>
     </div>
   );
-}
+};
 
 export default AppBar;
