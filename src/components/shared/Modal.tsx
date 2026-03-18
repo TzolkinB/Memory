6ff3efef0ebce0c81913b21253da2d7ef5d0a416
Kim Bell <krbell4@gmail.com>
2026-03-18 19:02:42 -0400

    Migrate Modal component to MDB React
    - Convert Bootstrap modal structure to MDB React components (MDBModal, MDBModalDialog, etc.)
    - Handle v7 prop changes: implement open state management and onClose handler
    - Add sophisticated jQuery integration bridge to maintain compatibility
    - Override jQuery modal() method to sync with React state changes
    - Convert buttons to MDBBtn components with appropriate props
    - Maintain data-testid for testing compatibility
    
    Modal now uses MDB React with preserved jQuery integration for Main.tsx

--- src/components/shared/Modal.tsx
+++ src/components/shared/Modal.tsx
@@ -1,4 +1,13 @@
 import React from 'react';
+import {
+  MDBModal,
+  MDBModalDialog,
+  MDBModalContent,
+  MDBModalHeader,
+  MDBModalBody,
+  MDBModalFooter,
+  MDBBtn,
+} from 'mdb-react-ui-kit';
 
 interface ModalProps {
   children: React.ReactNode;
@@ -11,6 +20,7 @@ interface JQueryElement {
   trigger: (event: string) => void;
   on: (event: string, handler: () => void) => void;
   off: (event: string, handler: () => void) => void;
+  modal: (action: string) => void;
 }
 
 type JQueryLike = (selector: string) => JQueryElement;
@@ -21,54 +31,77 @@ const getJQuery = (): JQueryLike | undefined => {
 };
 
 class Modal extends React.Component<ModalProps> {
+  state = {
+    open: false,
+  };
+
   componentDidMount() {
-    getJQuery()?.('#modal').trigger('focus');
+    // jQuery integration: listen for Bootstrap modal events and sync with MDB modal state
+    const $ = getJQuery();
+    if ($) {
+      const modalElement = $('#modal');
+
+      // Override jQuery modal method to control MDB modal
+      const originalModal = modalElement.modal;
+      modalElement.modal = (action: string) => {
+        if (action === 'hide') {
+          this.setState({ open: false });
+        } else if (action === 'show') {
+          this.setState({ open: true });
+        }
+        return originalModal?.call(modalElement, action);
+      };
+
+      // Listen for Bootstrap modal show events (triggered by data-toggle)
+      modalElement.on('show.bs.modal', () => {
+        this.setState({ open: true });
+      });
+    }
   }
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
 
   render(): React.JSX.Element {
     const { children, closeText, confirmText, handleClick } = this.props;
 
     return (
-      <div
-        className="modal fade"
-        id="modal"
-        tabIndex={-1}
-        role="dialog"
-        aria-hidden="true"
-        data-testid="restart-modal"
-      >
-        <div className="modal-dialog modal-dialog-centered" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <button
-                type="button"
-                className="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div className="modal-body pb-0 pl-5">{children}</div>
-            <div className="modal-footer p-3">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-dismiss="modal"
-              >
-                {closeText}
-              </button>
-              <button
-                type="button"
-                className="btn btn-raised btn-primary text-white"
-                onClick={handleClick}
-              >
-                {confirmText}
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <>
+        {/* Hidden div to maintain jQuery compatibility */}
+        <div id="modal" className="d-none" data-testid="restart-modal" />
+
+        <MDBModal
+          open={this.state.open}
+          onClose={this.handleClose}
+          tabIndex={-1}
+        >
+          <MDBModalDialog centered>
+            <MDBModalContent>
+              <MDBModalHeader>
+                <MDBBtn
+                  className="btn-close"
+                  color="none"
+                  onClick={this.handleClose}
+                ></MDBBtn>
+              </MDBModalHeader>
+              <MDBModalBody className="pb-0 pl-5">{children}</MDBModalBody>
+              <MDBModalFooter className="p-3">
+                <MDBBtn color="secondary" onClick={this.handleClose}>
+                  {closeText}
+                </MDBBtn>
+                <MDBBtn
+                  color="primary"
+                  className="btn-raised text-white"
+                  onClick={handleClick}
+                >
+                  {confirmText}
+                </MDBBtn>
+              </MDBModalFooter>
+            </MDBModalContent>
+          </MDBModalDialog>
+        </MDBModal>
+      </>
     );
   }
 }
